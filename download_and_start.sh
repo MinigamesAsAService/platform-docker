0299d97fdc4aa5e41576767dd1a942670688b17d
Murad Akhundov <muradakhdev@gmail.com>
2023-12-30 14:48:38 -0500

    new env var options

--- download_and_start.sh
+++ download_and_start.sh
@@ -7,10 +7,20 @@ if [[ -n "${CONFIG_PATH}" ]]; then
   echo "Unzipping file..."
   unzip config.zip
   rm config.zip
-else
-  echo "Please set the CONFIG_PATH environment variable."
-  exit 1
+elif [[ -n "${CONFIG_REPO}" ]]; then
+       git clone "${CONFIG_REPO}" .
 fi
+
+if [[ -n "${SKRIPT_PATH}" ]]; then
+  echo "Downloading config file from ${SKRIPT_PATH}..." &&
+  mkdir -p plugins/Skript/scripts/
+  curl -L "${SKRIPT_PATH}" -o script.zip
+  mv script.zip plugins/Skript/scripts/script.zip
+  echo "Unzipping file..."
+  cd plugins/Skript/scripts/ && unzip script.zip && rm script.zip && cd -
+fi
+
+
 
 # TODO: Read .minigame/config.toml for configurations on gamemode, etc.
 # See documentation at https://docker-minecraft-server.readthedocs.io/en/latest/configuration/server-properties
